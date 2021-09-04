f0614b29ef2d9cd77f5a67fe1eaa6d5815435af0
fanda521 <1056015243@qq.com>
2021-09-04 16:43:38 +0800

    在本地创建分支 1.0.1 ，自动显示Git:1.0.1 然后直接提交

---
+++ idea-git/src/main/java/com/wang/ideagit/entity/Student.java
@@ -0,0 +1,13 @@
+package com.wang.ideagit.entity;
+
+/**
+ * @version 1.0
+ * @Aythor lucksoul 王吉慧
+ * @date 2021/9/4 16:40
+ * @description 1.0.1
+ */
+public class Student {
+    private  String name;
+    private  Integer age;
+    private  String address;
+}
