06413adb54d096f57589ec06bb231be7c0cd01c4
jeffrey <1056015243@qq.com>
2022-04-27 11:20:26 +0800

    reset soft 1.新建测试Soft

---
+++ idea-git/src/main/java/com/wang/ideagit/reset/Soft.java
@@ -0,0 +1,13 @@
+package com.wang.ideagit.reset;
+
+/**
+ * @version 1.0
+ * @Aythor jeffrey 王吉慧
+ * @date 2022/4/27 11:18
+ * @description
+ */
+public class Soft {
+    private String name;
+
+    private int age;
+}
